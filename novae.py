5965da07807595ea072265e968f7284ac5db8ebc
Asier Aguado <asier.aguado@cern.ch>
2019-02-28 16:00:49 +0100

    Support python3

--- novae.py
+++ novae.py
@@ -1,5 +1,10 @@
 # coding=utf-8
-import urllib2
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
 from bs4 import BeautifulSoup
 import re
 import logging
@@ -17,10 +22,10 @@ class CernMenu:
     def __init__(self, in_english = False):
         if (in_english):
             self.lang = 'en'
-            content = urllib2.urlopen('http://www.novae-restauration.ch/menus/?x=ad3f8f75fe1e353b972afcce8e375d6e&y=81dc9bdb52d04dc20036dbd8313ed055&z=135&lang=en').read()
+            content = urlopen('http://www.novae-restauration.ch/menus/?x=ad3f8f75fe1e353b972afcce8e375d6e&y=81dc9bdb52d04dc20036dbd8313ed055&z=135&lang=en').read()
         else:
             self.lang = 'fr'
-            content = urllib2.urlopen('http://www.novae-restauration.ch/menus/?x=ad3f8f75fe1e353b972afcce8e375d6e&y=81dc9bdb52d04dc20036dbd8313ed055&z=135').read()
+            content = urlopen('http://www.novae-restauration.ch/menus/?x=ad3f8f75fe1e353b972afcce8e375d6e&y=81dc9bdb52d04dc20036dbd8313ed055&z=135').read()
         soup = BeautifulSoup(content, 'lxml')
         self.menu_types = soup.find_all(class_='typeMenu')
         self.dow = soup.find_all(class_='EnteteMenu')
@@ -36,7 +41,7 @@ class CernMenu:
         day = self.dow[downum]
         string += '<i>'+day.string+'</i>\n\n'
         for i in range(0,10):
-	    string += '<b>- '+self.menu_types[i].string+' -</b>\n'+re.sub('( [ \n])+', '', self.all_menus[10*downum+i].string)+'\n\n'
+            string += '<b>- '+self.menu_types[i].string+' -</b>\n'+re.sub('( [ \n])+', '', self.all_menus[10*downum+i].string)+'\n\n'
         return string
 
     def week_menu(self):
